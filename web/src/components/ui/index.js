c39f04b49dcaff2aeb92aa9b188bc774a36a9e41
skyback12 <adityamalik081203@gmail.com>
2025-09-15 16:16:46 +0530

    first commit

---
+++ web/src/components/ui/index.js
@@ -0,0 +1,7 @@
+// src/components/ui/index.js
+export { default as Button } from "./Button";
+export { default as Badge } from "./Badge";
+export { default as Card } from "./Card";
+export { default as Input } from "./Input";
+export { default as AnimatedCard } from "./AnimatedCard";
+export { default as AnimatedSection } from "./AnimatedSection";
