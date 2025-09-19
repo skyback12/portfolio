4234f95712e9047e5a6158638ebeb577738caa74
skyback12 <adityamalik081203@gmail.com>
2025-09-19 09:31:24 +0530

    final change for versel

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import LandingPage from "./pages/landingpage";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const router = createBrowserRouter([
   {
@@ -18,5 +20,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics/>
+    <SpeedInsights/>
   </React.StrictMode>
 );
