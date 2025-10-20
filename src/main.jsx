5265bba0c88d90c9edc5ec52ae8ae2def90040fa
Aditya malik <87317314+skyback12@users.noreply.github.com>
2025-10-20 17:24:19 +0530

    Updated tailwind.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App";
+import ModernPortfolio from "./pages/ModernPortfolio";
 import LandingPage from "./pages/landingpage";
-// 1. Import the ProjectOverview component
-import ProjectOverview from "./components/ProjectOverview"; 
+import ProjectOverview from "./components/ProjectOverview";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    children: [
-      { 
-        index: true, 
-        element: <LandingPage/> 
-      },
-      // 2. ADDED new route for project details
-      {
-        path: "project",
-        element: <ProjectOverview/>,
-      }
-    ],
+    element: <ModernPortfolio />
   },
+  {
+    path: "/classic",
+    element: <LandingPage />
+  },
+  {
+    path: "/project",
+    element: <ProjectOverview />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
